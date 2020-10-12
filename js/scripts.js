878835e8fddf0942d1a0350166eb7f4fe108fb2d
machnev_egor <46050097+machnevegor@users.noreply.github.com>
2020-10-12 20:26:42 +0300

    -
    +

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
+// # Site for -SM_TECHNOLOGY-
+// smtechnologyofficialcontact@gmail.com
+// # Authors of the project:
+// 1-MachnevEgor_https://vk.com/machnev_egor
+// 2-NasekinEvgeniy_https://vk.com/nasek1n
+// 3-DmitryShalimov_https://vk.com/lookatstat
+// # Contacts in email:
+// 1-meb.official.com@gmail.com
+// 2-nasekinwhiteorblack@gmail.com
+// 3-dmitriy-shalimov@yandex.ru
+
 // creating stars space
 window.onload = function(){
 		var canvas = document.createElement("canvas"),
@@ -48,3 +59,14 @@ window.onload = function(){
 			}
 		}, 30);
 	};
+
+// # Site for -SM_TECHNOLOGY-
+// smtechnologyofficialcontact@gmail.com
+// # Authors of the project:
+// 1-MachnevEgor_https://vk.com/machnev_egor
+// 2-NasekinEvgeniy_https://vk.com/nasek1n
+// 3-DmitryShalimov_https://vk.com/lookatstat
+// # Contacts in email:
+// 1-meb.official.com@gmail.com
+// 2-nasekinwhiteorblack@gmail.com
+// 3-dmitriy-shalimov@yandex.ru
